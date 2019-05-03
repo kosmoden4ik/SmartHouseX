2e217a33cfbf6abc71aea98a14db6cfbf90a712a
kosmoden4ik <kosmoden4ik@gmail.com>
2019-05-03 16:12:12 +0300

    NewBranch

---
+++ WebAPI/Models/DeviceModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI.Models
+{
+    public class DeviceModels
+    {
+          
+        public int id_Device { get; set; }
+        public string macadress { get; set; }
+        public string type_device { get; set; }
+        public string status { get; set; }//Comand
+        public DateTime last_activ_time { set; get; }
+    
+}
+}
